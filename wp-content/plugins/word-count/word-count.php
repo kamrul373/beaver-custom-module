810cd001e7e5b4e714790a376ec11caf80199fe8
kamrul373 <textile.zero@gmail.com>
2023-03-18 17:44:10 +0600

    total reading time feature included

--- wp-content/plugins/word-count/word-count.php
+++ wp-content/plugins/word-count/word-count.php
@@ -21,7 +21,29 @@ function word_counter_post_word_counter ($content) {
 	$label = apply_filters( "wordcounter_label", $label );
 	$tag = apply_filters( "wordcounter_tag", 'h2' );
 	$result = sprintf("<%s>%s %s</%s>",$tag,$label, $word_count, $tag);
-	return $result;
+	return $content .= $result;
 }
  add_filter("the_content",'word_counter_post_word_counter');
+
+function word_counter_post_reading_time ($content) {
+	$strip_content = strip_tags($content);
+	$word_count = str_word_count($strip_content);
+	$label = __("Total reading time", "word-counter");
+	$visible = apply_filters( "is_visible_post_reading_time", 1 );
+
+	if($visible) {
+		$minutes = floor($word_count / 200);
+		$seconds = floor(($word_count % 200)/(200/60));
+
+		$label = apply_filters( "wordcounter_reading_time_label", $label );
+		$tag = apply_filters( "wordcounter_reading_time_label_tag", 'h5' );
+		
+		$result = sprintf("<%s> %s : %s minutes %s seconds </%s>", $tag,$label, $minutes, $seconds, $tag);
+		return $content .= $result;
+	}
+	return $content;
+
+}
+
+ add_filter("the_content","word_counter_post_reading_time");
 
